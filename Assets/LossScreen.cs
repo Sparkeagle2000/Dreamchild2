93eaac30ddeb1f502ea4544ccf225308aad7fb9e
Mjplace23 <mjplace.bass@knights.ucf.edu>
2021-04-22 16:45:51 -0400

    Loss Scene
    Added the game over screen and the code for it is connected

---
+++ Assets/LossScreen.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LossScreen : MonoBehaviour
+{
+    public void HubLevel()
+    {
+        SceneManager.LoadScene("Hub");
+    }
+
+    public void Restart()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
